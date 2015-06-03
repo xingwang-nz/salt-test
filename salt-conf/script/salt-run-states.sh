bb561a49e307c9ddf1baf2adfc4f7c91c54f5846
xing-invenco <xing.wang@invenco.com>
2015-06-03 15:08:06 +1200

    test

--- salt-conf/script/salt-run-states.sh
+++ salt-conf/script/salt-run-states.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-if [ -n "$1" ]; then
-   sudo salt "$1" state.highstate
+if [ "$#" -eq 1 ]; then
+    sudo salt "$1" state.highstate
+elseif [ "$#" -eq 2 ]; then
+    sudo salt -L "$1","$2" state.highstate
 else
-#  sudo salt '*' state.highstate
-   echo "usage: salt-run-state.sh <minion_id>"
+   echo "usage: salt-run-state.sh <minion1_id> <minion2_id> or salt-run-state.sh <minion1_id>"
 fi
 
