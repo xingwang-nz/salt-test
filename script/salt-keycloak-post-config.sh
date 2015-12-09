fa5fd61b9f354c6e7c6f4979309f3750eb2f308a
xing-invenco <xing.wang@invenco.com>
2015-12-09 16:03:04 +1300

    test

--- script/salt-keycloak-post-config.sh
+++ script/salt-keycloak-post-config.sh
@@ -7,7 +7,7 @@ else
 	pillar_value="{\"keycloak_admin_current_password\": \""$2"\"}"
 	read -p "Are you sure you want to run this salt state, the admin password will be updated (yY/nN)?" choice
 	case "$choice" in
-      y|Y ) sudo salt "$1" state.sls salt-keycloak-post-config pillar="$pillar_value";;
+      y|Y ) sudo salt "$1" state.sls keycloak-post-config pillar="$pillar_value";;
       * ) exit 1;;
      esac
 fi
