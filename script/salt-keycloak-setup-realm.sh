08cf323393a20aa4bd16358fc97b64566726b7f3
xing-invenco <xing.wang@invenco.com>
2016-01-20 14:44:45 +1300

    test

--- script/salt-keycloak-setup-realm.sh
+++ script/salt-keycloak-setup-realm.sh
@@ -2,7 +2,7 @@
 if [ -n "$1" ]; then
      read -p 'Keycloak master admin username: ' kc_master_admin_username
      read -sp 'Keycloak master admin password: ' kc_master_admin_password
-     pillar_value="{\"setup_kc_realm\": \"True\", \"kc_master_admin_username\": \""$kc_master_admin_username"\", \"kc_master_admin_password\": \""$kc_master_admin_password"\"}"
+     pillar_value="{\"setup_kc_realm\": \"True\", \"force_reload_keycloak_config\": \"True\", \"kc_master_admin_username\": \""$kc_master_admin_username"\", \"kc_master_admin_password\": \""$kc_master_admin_password"\"}"
      echo ""
 	sudo salt "$1" state.sls keycloak-setup-realm pillar="$pillar_value"
 else
