5f45686f4577068bfec779be679f78cc68e9fc1b
xing-invenco <xing.wang@invenco.com>
2016-01-20 13:11:34 +1300

    test

--- script/salt-keycloak-setup-realm.sh
+++ script/salt-keycloak-setup-realm.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [ -n "$1" ]; then
-	pillar_value="{\"force_reload_keycloak_config\": \"True\"}"
-	sudo salt "$1" state.sls keycloak-create-realm pillar="$pillar_value"
+     read -p 'Keycloak master admin username: ' kc_master_admin_username
+     read -sp 'Keycloak master admin password: ' kc_master_admin_password
+     pillar_value="{\"setup_kc_realm\": \"True\", \"kc_master_admin_username\": \""$kc_master_admin_username"\", \"kc_master_admin_password\": \""$kc_master_admin_password"\"}"
+     echo ""
+	sudo salt "$1" state.sls keycloak-setup-realm pillar="$pillar_value"
 else
 	echo "usage: salt-create-realm.sh <minion_id>"
 fi
--- script/salt-run-states.sh
+++ script/salt-run-states.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 if [ "$#" -eq 1 ]; then
     sudo salt "$1" state.highstate
-elseif [ "$#" -eq 2 ]; then
-    sudo salt -L "$1","$2" state.highstate
+elif [ "$#" -eq 2 ]; then
+   if [ "$2" = "--setup-kc-realm" ]; then
+     read -p 'Keycloak master admin username: ' kc_master_admin_username
+     read -sp 'Keycloak master admin password: ' kc_master_admin_password
+     pillar_value="{\"setup_kc_realm\": \"True\", \"kc_master_admin_username\": \""$kc_master_admin_username"\", \"kc_master_admin_password\": \""$kc_master_admin_password"\"}"
+     echo ""
+     sudo salt "$1" state.highstate  pillar="$pillar_value"
+   else
+     echo "usage: salt-run-state.sh <minion1_id> [--setup-kc-realm] or salt-run-state.sh <minion1_id>"
+   fi
 else
-   echo "usage: salt-run-state.sh <minion1_id> <minion2_id> or salt-run-state.sh <minion1_id>"
+   echo "usage: salt-run-state.sh <minion1_id> [--setup-kc-realm] or salt-run-state.sh <minion1_id>"
 fi
 
